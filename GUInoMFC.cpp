97628df6ba90e7b439324adfc5ba8e520bad88e7
NguyenThanhTrung-B20AT195 <147567991+NguyenThanhTrung-B20AT195@users.noreply.github.com>
2025-07-10 21:39:31 +0700

    Add files via upload

---
+++ GUInoMFC.cpp
@@ -0,0 +1,80 @@
+﻿#include <windows.h>
+#include <string>
+#include <cwctype> // cho iswupper, towupper.
+
+#define ID_EDIT       1
+#define ID_BUTTON     2
+#define ID_CHECKBOX   3
+#define ID_OUTPUT     4
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+std::wstring ReverseAndToggleCase(const std::wstring& input, bool toggleCase) {
+    std::wstring result(input.rbegin(), input.rend());
+    if (toggleCase) {
+        for (wchar_t& c : result) {
+            if (iswlower(c)) c = towupper(c);
+            else if (iswupper(c)) c = towlower(c);
+        }
+    }
+    return result;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
+    const wchar_t CLASS_NAME[] = L"ReverseApp";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hbrBackground = (HBRUSH)(COLOR_INACTIVECAPTION + 1);
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0, CLASS_NAME, L"Reverse Text App",
+        WS_OVERLAPPEDWINDOW,
+        750, 350, 365, 160,
+        nullptr, nullptr, hInstance, nullptr
+    );
+    if (!hwnd) return 0;
+    ShowWindow(hwnd, nCmdShow);
+    // Message loop
+    MSG msg = {};
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    static HWND hEdit, hButton, hCheckBox, hResult;
+
+    switch (uMsg) {
+    case WM_CREATE:
+        hEdit = CreateWindow(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
+            5, 10, 340, 25, hwnd, (HMENU)ID_EDIT, nullptr, nullptr);
+        hCheckBox = CreateWindow(L"BUTTON", L"UpCase / LowCase", WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX,
+            5, 45, 200, 20, hwnd, (HMENU)ID_CHECKBOX, nullptr, nullptr);
+        hButton = CreateWindow(L"BUTTON", L"Reverse", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+            225, 44, 120, 22, hwnd, (HMENU)ID_BUTTON, nullptr, nullptr);
+        hResult = CreateWindow(L"EDIT", L"", WS_CHILD | WS_VISIBLE| ES_READONLY | ES_AUTOHSCROLL,
+            5, 80, 340, 25, hwnd, (HMENU)ID_OUTPUT, nullptr, nullptr);
+        break;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == ID_BUTTON) {
+            wchar_t buffer[256] = {};
+            GetWindowText(hEdit, buffer, 256);
+            bool toggleCase = (SendMessage(hCheckBox, BM_GETCHECK, 0, 0) == BST_CHECKED);
+            std::wstring result = ReverseAndToggleCase(buffer, toggleCase);
+            SetWindowText(hResult, result.c_str());
+        }
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
---
+++ RegistrySet.cpp
@@ -0,0 +1,190 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+
+void ReadRegistryValue(HKEY hKeyRoot, LPCSTR subKey, LPCSTR valueName) {
+    HKEY hKey;
+    BYTE value[255];
+    DWORD value_length = sizeof(value);
+    DWORD type;
+
+    if (RegOpenKeyExA(hKeyRoot, subKey, 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+        if (RegGetValueA(hKey, NULL, valueName, RRF_RT_ANY, &type, &value, &value_length) == ERROR_SUCCESS) {
+            if (type == REG_DWORD) {
+                DWORD dwordValue;
+                memcpy(&dwordValue, value, sizeof(DWORD));
+                std::cout << "Value: " << dwordValue << std::endl;
+            }
+            else if (type == REG_BINARY) {
+                for (DWORD i = 0; i < value_length; ++i) {
+                    for (int bit = 7; bit >= 0; --bit) {
+                        std::cout << ((value[i] >> bit) & 1);
+                    }
+                    std::cout << " ";
+                }
+            }
+            else if (type == REG_SZ) std::cout << "Value: " << (char*)value << std::endl;
+        }
+        else {
+            std::cout << "Failed to read value.\n";
+        }
+        RegCloseKey(hKey);
+    }
+    else {
+        std::cout << "Failed to open registry key.\n";
+    }
+}
+
+void CreateRegistryKey(HKEY hKeyRoot, LPCSTR subKey, LPCSTR valueName, LPCSTR data) {
+    HKEY hKey;
+    if (RegCreateKeyExA(hKeyRoot, subKey, 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
+        if (RegSetValueExA(hKey, valueName, 0, REG_SZ, (const BYTE*)data, strlen(data) + 1) == ERROR_SUCCESS) {
+            std::cout << "Key and value created successfully.\n";
+        }
+        else {
+            std::cout << "Failed to set value.\n";
+        }
+        RegCloseKey(hKey);
+    }
+    else {
+        std::cout << "Failed to create key.\n";
+    }
+}
+
+void DeleteRegistryKey(HKEY hKeyRoot, LPCSTR subKey) {
+    HKEY hKey;
+    if (RegCreateKeyExA(hKeyRoot, subKey, 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
+
+    }
+    else {
+        std::cout << "Failed to open\n";
+    }
+    if (RegDeleteKeyA(hKeyRoot, subKey) == ERROR_SUCCESS) {
+        std::cout << "Key deleted successfully.\n";
+    }
+    else {
+        std::cout << "Failed to delete key.\n";
+    }
+}
+
+void TurnOnAutoStart(HKEY hKeyRoot, LPCSTR subKey, LPCSTR valueName, LPCSTR data) {
+    HKEY hKey;
+    if (RegCreateKeyExA(hKeyRoot, subKey, 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
+        if (RegSetValueExA(hKey, valueName, 0, REG_SZ, (const BYTE*)data, strlen(data) + 1) == ERROR_SUCCESS) {
+            std::cout << "Turn on auto start with windows.\n";
+        }
+        else {
+            std::cout << "Failed to set key.\n";
+        }
+        RegCloseKey(hKey);
+    }
+    else {
+        std::cout << "Failed to create auto start.\n";
+    }
+}
+
+void TurnOffAutoStart(HKEY hKeyRoot, LPCSTR subKey, LPCSTR valueName) {
+    HKEY hKey;
+    if (RegOpenKeyExA(hKeyRoot, subKey, 0, KEY_WRITE, &hKey) == ERROR_SUCCESS) {
+        if (RegDeleteValueA(hKey, valueName) == ERROR_SUCCESS) {
+            std::cout << "Turn off auto start with windows.\n";
+        }
+        else {
+            std::cout << "Failed to Turn off auto start.\n";
+        }
+    }
+    else {
+        std::cout << "Failed to delete auto start.\n";
+    }
+}
+
+bool AutoStartState(HKEY hKeyRoot, LPCSTR subKey, LPCSTR valueName, LPCSTR data) {
+    HKEY hKey;
+    BYTE value[255];
+    DWORD value_length = sizeof(value);
+    DWORD type;
+    bool c = true;
+    std::string a = data;
+    if (RegOpenKeyExA(hKeyRoot, subKey, 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+        if (RegGetValueA(hKey, NULL, valueName, RRF_RT_ANY, &type, &value, &value_length) == ERROR_SUCCESS) {
+            if ((char*)value != a) c = false;
+        }
+        else {
+            c = false;
+        }
+        RegCloseKey(hKey);
+    }
+    else {
+        c = false;
+    }
+    if(c==true) std::cout << "ON\n";
+    else std::cout << "OFF\n";
+    return c;
+}
+
+int main() {
+    std::string subKey = "";
+    std::string valueName = "";
+    std::string data = "";
+    HKEY AhKey = HKEY_CURRENT_USER;
+    std::string AsubKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+    std::string AvalueName = "Auto Start Edit REG_KEY";
+    std::string Adata = "D:\\VS\\Project\\Project1\\Debug\\Project1.exe";
+    while (true) {
+        std::cout << "\n------***------\n";
+        std::cout << "1. Create key\n2. Read key value\n3. Delete key\n4. Turn on/off auto start\n0. Exit\nState of automatic start: ";
+        bool c = AutoStartState(AhKey, AsubKey.c_str(), AvalueName.c_str(), Adata.c_str());
+        std::cout << "---------------\nChon: ";
+        int chon;
+        std::cin >> chon;
+        if (chon == 0) return 0;
+        if (chon == 4) {
+            if (c!=true) {
+                TurnOnAutoStart(AhKey, AsubKey.c_str(), AvalueName.c_str(), Adata.c_str());
+            }
+            else {
+                
+                TurnOffAutoStart(AhKey, AsubKey.c_str(), AvalueName.c_str());
+            }
+        }
+        if (chon > 0 && chon < 4) {
+            std::cout << "Press any opion in here:\n";
+            std::cout << "1.HKEY_CLASSES_ROOT   2.HKEY_CURRENT_USER   3.HKEY_LOCAL_MACHINE   4.HKEY_USERS   5.HKEY_CURRENT_CONFIG\nChon: ";
+            int choice;
+            std::cin >> choice;
+            HKEY hKey = (choice == 1) ? HKEY_CLASSES_ROOT :
+                (choice == 2) ? HKEY_CURRENT_USER :
+                (choice == 3) ? HKEY_LOCAL_MACHINE :
+                (choice == 4) ? HKEY_USERS :
+                (choice == 5) ? HKEY_CURRENT_CONFIG :
+                NULL;
+            std::cin.ignore();
+            std::cout << "Enter subKey (defaut NULL): ";
+            std::getline(std::cin, subKey);
+            if (chon != 3) std::cout << "Enter valueName (defaut NULL): ";
+            if (chon != 3) std::getline(std::cin, valueName);
+            if (chon == 1) std::cout << "Enter data (defaut NULL): ";
+            if (chon == 1) std::getline(std::cin, data);
+            
+            char check;
+            // Tạo khóa và giá trị
+            if (chon == 1) CreateRegistryKey(hKey, subKey.c_str(), valueName.c_str(), data.c_str());
+            // Đọc giá trị
+            if (chon == 2) ReadRegistryValue(hKey, subKey.c_str(), valueName.c_str());
+            // Xóa khóa
+            if (chon == 3) {
+                char check;
+                std::string rootKey = (choice == 1) ? "HKEY_CLASSES_ROOT" :
+                    (choice == 2) ? "HKEY_CURRENT_USER" :
+                    (choice == 3) ? "HKEY_LOCAL_MACHINE" :
+                    (choice == 4) ? "HKEY_USERS" :
+                    (choice == 5) ? "HKEY_CURRENT_CONFIG" :
+                    NULL;
+                std::cout << "Press y to delete key *" << rootKey << '\\' << subKey << "*: ";
+                std::cin >> check;
+                if (check == 'y') DeleteRegistryKey(hKey, subKey.c_str());
+            }
+        }
+    }
+    return 0;
+}
